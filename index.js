0fcb5c8c0c1f7fd8cfb812f41e9935f338a65da9
Ismael Valentin <ismaelvale@gmail.com>
2022-07-28 11:07:48 -0400

    Refactored Node to use closure.

--- index.js
+++ index.js
@@ -7,36 +7,16 @@ const port = process.env.PORT || 8080
 const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
-            fs.readFile(path.join(__dirname,'index.html'), (err, content) => {
-            if (err) throw err;
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(content);
-        });
+            fs.readFile(path.join(__dirname,'index.html'), buildDisplay(res, 200));
         break;
     case '/about':
-        fs.readFile(path.join(__dirname,'about.html'), (err, content) => {
-            if (err) throw err;
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(content);
-        });
+        fs.readFile(path.join(__dirname,'about.html'), buildDisplay(res, 200));
         break;
     case '/contact-me':
-        fs.readFile(path.join(__dirname,'contact-me.html'), (err, content) => {
-            if (err) throw err;
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(content);
-        });
+        fs.readFile(path.join(__dirname,'contact-me.html'), buildDisplay(res, 200));
         break;
     default:
-        fs.readFile(path.join(__dirname,'404.html'), (err, content) => {
-            if (err) throw err;
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(content);
-        });
+        fs.readFile(path.join(__dirname,'404.html'), buildDisplay(res, 404));
         break;
     };
 });
@@ -44,3 +24,13 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
     console.log(`Server running at port ${port}`)
 });
+
+function buildDisplay(res, statusCode) {
+    return function display (err, content) {
+        if (err) throw err;
+        res.statusCode = statusCode;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(content);
+    };
+};
+
